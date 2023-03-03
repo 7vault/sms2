7518fae49ab987bd890ae8fc797e08d3f5689d7e
Jennifer Bourdeau <jenniferbourdeau15@gmail.com>
2023-03-03 11:02:21 -0900

    coding part

---
+++ Cours.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sms2
+{
+    public class Cours
+    {
+
+        private String _nomCours; String _cours_ID; String _dd_Cours; int _credits; String _horaire;
+        public Cours(string NomCours, String Cours_ID, String DD_Cours, int Credits, String Horaire)
+        {
+            this.NomCours = NomCours;
+            this.Cours_ID = Cours_ID;
+            this.DD_Cours = DD_Cours;
+            this.Credits = Credits;
+            this.Horaire = Horaire;
+        }
+
+        public string NomCours
+        {
+            get => NomCours;
+            set => NomCours = value.First().ToString().ToUpper() + value.Substring(1);
+        }
+
+        public string Cours_ID
+        {
+            get => Cours_ID;
+            set => Cours_ID = value;
+        }
+
+        public string DD_Cours
+        {
+            get => DD_Cours;
+            set => DD_Cours = value;
+        }
+
+        public int Credits
+        {
+            get => Credits;
+            set => Credits = value;
+        }
+
+        public string Horaire
+        {
+            get => Horaire;
+            set => Horaire = value;
+        }
+
+
+
+
+
+    }
+}
---
+++ Etudiant.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sms2
+{
+    public class Etudiant
+    {
+
+        private String _nom; String _prenom; String _ddN; String _nationalite; String _sexe; String _adresse; String email; String _telephone; String _discipline; String _etudiant_ID; String _dateCree;
+
+
+        public Etudiant(string Nom, String Prenom, String DDN, String Nationalite, String Sexe, String Adresse, String Email, String Telephone, String Discipline, String Etudiant_ID, String DateCree)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+            this.DDN = DDN;
+            this.Nationalite = Nationalite;
+            this.Sexe = Sexe;
+            this.Adresse = Adresse;
+            this.Email = Email;
+            this.Telephone = Telephone;
+            this.Discipline = Discipline;
+            this.Etudiant_ID = Etudiant_ID;
+            this.DateCree = DateCree;
+
+
+
+        }
+
+
+        public string Nom
+        {
+            get => _nom;
+            set => _nom = value.First().ToString().ToUpper() + value.Substring(1);
+        }
+
+        public string Prenom
+        {
+            get => _prenom;
+            set => _prenom = value.First().ToString().ToUpper() + value.Substring(1);
+
+        }
+        public string DDN
+        {
+            get => DDN;
+            set => DDN = value;
+        }
+
+
+        public string Nationalite
+        {
+            get => _nationalite;
+            set => _nationalite = value.First().ToString().ToUpper() + value.Substring(1);
+
+        }
+
+        public string Sexe
+        {
+            get => Sexe;
+            set => Sexe = value;
+        }
+
+        public string Adresse
+        {
+            get => _adresse;
+            set => _adresse = value;
+        }
+
+        public string Email
+        {
+            get => Email;
+            set => Email = value;
+        }
+
+        public string Telephone
+        {
+            get => _telephone;
+            set => _telephone = value;
+        }
+
+        public string Discipline
+        {
+            get => Discipline;
+            set => Discipline = value.First().ToString().ToUpper() + value.Substring(1);
+        }
+        public string Etudiant_ID
+        {
+            get => Etudiant_ID;
+            set => Etudiant_ID = value;
+
+        }
+
+        public string DateCree
+        {
+            get => _dateCree;
+            set => _dateCree = DateTime.Now.ToString("dd-MM-yyyy");
+        }
+
+        public override string ToString()
+        {
+            return Nom + " " + Prenom;
+        }
+
+    }
+
+}
